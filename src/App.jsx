329dd5ab0308ccc5492a58b650755715e58d8f4c
Ashish <kapoorashish713@gmail.com>
2025-07-31 13:46:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className=" min-h-screen min-w-screen flex items-center justify-center">
+      app
+    </div>
+  );
+}
+
+export default App;
